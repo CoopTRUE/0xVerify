5bf75c1b9cb92cba59e19c46a71890d03757a62a
coopTRUE <coopdog4545@gmail.com>
2023-04-05 13:43:42 -0700

    initial files

---
+++ src/app.d.ts
@@ -0,0 +1,14 @@
+// See https://kit.svelte.dev/docs/types#app
+// for information about these interfaces
+declare global {
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface Platform {}
+  }
+  // eslint-disable-next-line no-var
+  var test: Record<string, Record<56 | 55 | 54>, boolean>
+}
+
+export {}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+</script>
+
+<svelte:head>
+  <title>0xVerify</title>
+  <meta
+    name="description"
+    content="A quick and easy way create and verify ethereum wallet ownership."
+  />
+</svelte:head>
+
+<main>
+  <h1>hi</h1>
+</main>
+
+<style lang="scss">
+  h1 {
+    color: green;
+  }
+</style>
