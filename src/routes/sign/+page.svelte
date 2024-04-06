2e90c2af4c7c81bf9bb8dad816c7f33007169de1
CoopTRUE <coopdog4545@gmail.com>
2024-04-06 13:48:09 -0700

    starting work on signature

--- src/routes/sign/+page.svelte
+++ src/routes/sign/+page.svelte
@@ -23,17 +23,17 @@
 {#key loaded}
   <main class={cn('flex flex-col items-center opacity-0', loaded && 'opacity-100')}>
     <div in:slide={{ axis: 'x', duration: 900, easing: cubicInOut }} class="pb-6 sm:pb-5">
-      <h1 class="font-['Orbitron'] text-[clamp(2.5rem,15vw,5rem)] font-black leading-none">
-        0xVerify
+      <h1 class="mb-2 font-['Orbitron'] text-[clamp(2.5rem,15vw,5rem)] font-black leading-none">
+        0xSign
       </h1>
       <p class="text-nowrap text-[clamp(0.6rem,1.1rem,4vw)] font-medium text-muted-foreground">
         Cryptographic signature verification <Button
           variant="link"
           size="sm"
           class="p-0 text-lg"
-          href="/sign"
+          href="/verify"
         >
-          or sign
+          or verify
         </Button>
       </p>
     </div>
---
+++ src/routes/sign/schema.ts
@@ -0,0 +1,17 @@
+import type { Hex, Address } from 'viem'
+import { z } from 'zod'
+
+export const formSchema = z.object({
+  message: z.string().min(1, { message: 'Message cannot be empty' }),
+  signatureHash: z
+    .string()
+    .regex(/^0x([A-Fa-f0-9]{130})$/, { message: 'Invalid signature hash' })
+    .transform((v) => v as Hex),
+  address: z
+    .string()
+    .refine((v) => !v || /^0x([A-Fa-f0-9]{40})$/.test(v), { message: 'Invalid address' })
+    .transform((v) => v as Address)
+    .optional(),
+})
+
+export type FormSchema = typeof formSchema
